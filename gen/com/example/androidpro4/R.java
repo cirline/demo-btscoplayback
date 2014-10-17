56b421dc20c74889a65a4eb2c02210273f03c8a6
kangear <kangear@163.com>
2014-10-17 12:52:08 +0800

    gitignore

--- gen/com/example/androidpro4/R.java
+++ gen/com/example/androidpro4/R.java
@@ -25,12 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int TextView01=0x7f080003;
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080003;
         public static final int btnExit=0x7f080002;
         public static final int btnRecord=0x7f080000;
         public static final int btnStop=0x7f080001;
-        public static final int skbVolume=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -41,6 +39,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int attention=0x7f050006;
         public static final int btnE=0x7f050004;
         public static final int btnR=0x7f050002;
         public static final int btnS=0x7f050003;
